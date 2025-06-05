c6b25c7fdd3021eef0dabeaedfc9cf5bb702f652
samiyariya <samiya.riya16@gmail.com>
2025-06-05 07:43:34 +0600

    4

--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -11,10 +11,10 @@ const NewsCard = ({ news }) => {
         navigate(`/news/${news.id}`);
         scrollTo(0, 0);
       }}
-      className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-300 w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.333rem)] min-w-[280px]"
+      className="border rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-300 w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.333rem)] min-w-[280px] shadow-[0_8px_25px_-5px_rgba(0,0,0,0.1),0_8px_10px_-6px_rgba(0,0,0,0.1)] hover:shadow-[0_20px_40px_-10px_rgba(0,0,0,0.2),0_15px_20px_-8px_rgba(0,0,0,0.15)]"
     >
       <img
-        className="w-full h-40 object-cover bg-blue-50"
+        className="w-full h-60 object-cover bg-blue-50"
         src={news.image}
         alt={news.title}
       />
@@ -25,12 +25,12 @@ const NewsCard = ({ news }) => {
           {news.description.slice(0, 80)}...
         </p>
         <div className="text-xs text-gray-500 mt-4 space-y-1">
-          <p>
+          {/* <p>
             <strong>Location:</strong> {news.location}
-          </p>
-          <p>
+          </p> */}
+          {/* <p>
             <strong>Time:</strong> {news.time}
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -3,7 +3,7 @@ import { sampleNews } from '../assets/assets';
 
 const News = () => {
   return (
-    <div className="m-8 max-h-[90vh] overflow-y-scroll">
+    <div className="m-14 max-h-[90vh] overflow-y-scroll">
       <h1 className="text-lg font-medium">All News</h1>
       <div className="w-full flex flex-wrap gap-8 pt-5 gap-y-8">
         {sampleNews.map((item) => (
