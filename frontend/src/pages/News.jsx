4621db6811bc625a899363f1e834e1de30c2c0be
samiyariya <samiya.riya16@gmail.com>
2025-06-05 07:15:57 +0600

    news

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import NewsCard from '../components/NewsCard';
+import { sampleNews } from '../assets/assets';
 
 const News = () => {
   return (
-    <div>News</div>
-  )
-}
+    <div className="m-5 max-h-[90vh] overflow-y-scroll">
+      <h1 className="text-lg font-medium">All News</h1>
+      <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
+        {sampleNews.map((item) => (
+          <NewsCard key={item.id} news={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default News
+export default News;
