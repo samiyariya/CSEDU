d8f01297a43328eca76685072d5c82c71b118183
samiyariya <samiya.riya16@gmail.com>
2025-06-05 14:59:15 +0600

    good 3

--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -11,10 +11,10 @@ const NewsCard = ({ news }) => {
         navigate(`/news/${news.id}`);
         scrollTo(0, 0);
       }}
-      className="border rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-300 w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.333rem)] min-w-[280px] shadow-[0_8px_25px_-5px_rgba(0,0,0,0.1),0_8px_10px_-6px_rgba(0,0,0,0.1)] hover:shadow-[0_20px_40px_-10px_rgba(0,0,0,0.2),0_15px_20px_-8px_rgba(0,0,0,0.15)] font-['Poppins'] bg-blue-50"
+      className="border rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-300 w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.333rem)] min-w-[280px] shadow-[0_8px_25px_-5px_rgba(0,0,0,0.1),0_8px_10px_-6px_rgba(0,0,0,0.1)] hover:shadow-[0_20px_40px_-10px_rgba(0,0,0,0.2),0_15px_20px_-8px_rgba(0,0,0,0.15)] font-['Poppins'] bg-gray-50 hover:bg-blue-50"
     >
       <img
-        className="w-full h-60 object-cover bg-blue-50"
+        className="w-full h-60 object-cover bg-gray-50"
         src={news.image}
         alt={news.title}
       />
